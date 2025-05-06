316889147d2feec63fd716d619f7b3f6f2ca5aba
Alejandro <alejandroo.polo@gmail.com>
2025-05-06 18:29:02 +0200

    funcion de cambiar el aspecto de la app

--- Gamehub/src/app/features/inicio/settings/settings.component.ts
+++ Gamehub/src/app/features/inicio/settings/settings.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SideNavbarComponent } from "../../../services/side-navbar/side-navbar.component";
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [SideNavbarComponent],
+  imports: [SideNavbarComponent ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
-export class SettingsComponent {
-
+export class SettingsComponent implements OnInit{
+  temaActual : string = '';
+  ngOnInit(){
+    const outlet = document.querySelector('router-outlet');
+    if(outlet){
+      const tema = outlet.getAttribute('body');
+      this.temaActual = (tema === 'light' || tema === 'dark') ? tema : 'dark';
+      localStorage.setItem('tema' , this.temaActual);
+    }
+  }
+  cambiarTema(event : Event){
+    const valor = (event.target as HTMLSelectElement).value;
+    const outlet = document.querySelector('body');
+    console.log(valor)
+    if(outlet){
+      outlet.setAttribute('data-bs-theme', valor);
+      localStorage.setItem('tema' , valor);
+    }
+  }
 }
