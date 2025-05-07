68fbf94065f80ad1bd5c5e0897d7f2880f8c19a3
Alejandro <alejandroo.polo@gmail.com>
2025-05-07 19:47:59 +0200

    Se ha cambiado la ruta de la BBDD

--- ApiRest/restServiceGamehub/services.php
+++ ApiRest/restServiceGamehub/services.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //importamos la conexion con la bbddd
-require_once('./config/conexionBBDD.php');
+require_once('/config/conexionBBDD.php');
 //Importamos JWT
 require 'vendor/autoload.php';
 
